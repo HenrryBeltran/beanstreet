f79bbb4a2100cccc104c8bc2efdf5c1e9342c1ca
Henrry Beltrán <henrry26bt@gmail.com>
2023-10-28 19:50:45 -0500

    feat: Add Poppins & Libre Bodoni Fonts

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,13 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center"></main>
+    <main className="flex min-h-screen font-sans flex-col items-center justify-center">
+      <h1 className="font-serif text-5xl font-bold">Bean Street</h1>
+      <p className="">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga odit enim,
+        debitis suscipit culpa animi voluptates dolore perferendis dolores,
+        facere illum earum deleniti voluptatibus magni aspernatur. Deleniti,
+        fuga. Tempore, consequatur.
+      </p>
+    </main>
   );
 }
