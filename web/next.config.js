8df2967f54efd77e64577562ed2c618a568d79d3
Henrry Beltrán <henrry26bt@gmail.com>
2023-12-04 19:38:34 -0500

    fix: Change next configuration for production

--- web/next.config.js
+++ web/next.config.js
@@ -3,9 +3,9 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3000",
+        protocol: "https",
+        hostname: "beanstreet.pages.dev",
+        port: "",
         pathname: "/**",
       },
     ],
