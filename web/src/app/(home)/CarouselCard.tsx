d1ef84c7326a8c8e0bf3359ed839a11c468af382
Henrry Beltrán <henrry26bt@gmail.com>
2023-12-10 13:46:04 -0500

    feat: Add images priority on load

--- web/src/app/(home)/CarouselCard.tsx
+++ web/src/app/(home)/CarouselCard.tsx
@@ -1,40 +1,46 @@
 import { Item } from "@/lib/getSelectedItems";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function CarouselCard({ item }: { item: Item }) {
   return (
-    <li className="relative w-fit snap-start space-y-3 pb-3">
-      <Image
-        src={`${process.env.SITE_URL}/items/${item.slug}.jpg`}
-        width={320}
-        height={320}
-        loading="eager"
-        className="aspect-square bg-stone-300 object-cover object-center"
-        alt={item.name}
-      />
-      <h3 className="font-serif text-xl font-bold leading-none text-stone-800">
-        {item.name}
-      </h3>
-      <p className="text-sm leading-none text-stone-500">{item.section_name}</p>
-      <div className="flex items-baseline justify-between">
-        <div className="space-x-3 font-medium">
-          {item.price_w_discount ? (
-            <span className="leading-none text-stone-500 line-through">
-              ${item.price}
-            </span>
-          ) : (
-            <span className="leading-none text-stone-700">${item.price}</span>
-          )}
+    <li className="relative w-fit snap-start">
+      <Link
+        className="block w-full space-y-3 pb-3 tap-highlight-transparent"
+        href={`/shop/${item.slug}`}
+      >
+        <Image
+          src={`${process.env.SITE_URL}/items/${item.slug}.jpg`}
+          width={320}
+          height={320}
+          priority={true}
+          className="aspect-square bg-stone-300 object-cover object-center"
+          alt={item.name}
+        />
+        <h3 className="font-serif text-xl font-bold leading-none text-stone-800">
+          {item.name}
+        </h3>
+        <p className="text-sm leading-none text-stone-500">{item.section_name}</p>
+        <div className="flex items-baseline justify-between">
+          <div className="space-x-3 font-medium">
+            {item.price_w_discount ? (
+              <span className="leading-none text-stone-500 line-through">
+                ${item.price}
+              </span>
+            ) : (
+              <span className="leading-none text-stone-700">${item.price}</span>
+            )}
+            {item.price_w_discount && (
+              <span className="leading-none text-red-500">${item.price_w_discount}</span>
+            )}
+          </div>
           {item.price_w_discount && (
-            <span className="leading-none text-red-500">${item.price_w_discount}</span>
+            <span className="rounded-full bg-orange-100 px-3 py-1.5 text-sm leading-none text-orange-600">
+              -{item.discount}%
+            </span>
           )}
         </div>
-        {item.price_w_discount && (
-          <span className="rounded-full bg-orange-100 px-3 py-1.5 text-sm leading-none text-orange-600">
-            -{item.discount}%
-          </span>
-        )}
-      </div>
+      </Link>
     </li>
   );
 }
