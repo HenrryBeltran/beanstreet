e861132fd23a89986d55e0b656c5ade0299dd7e0
Henrry Beltrán <henrry26bt@gmail.com>
2023-12-14 13:23:58 -0500

    fix(web): Upadate

--- web/src/lib/getSelectedItems.ts
+++ web/src/lib/getSelectedItems.ts
@@ -17,23 +17,31 @@ export type Item = {
 type GetHandler = (type: string) => Promise<SelectedCoffees | null>;
 
 export const getSelectedItems: GetHandler = async type => {
-  const { error: fetchError, result: response } = await Try(
-    fetch(`${process.env.NEXT_PUBLIC_API_URL}/item/selected/${type}`, {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/item/selected/${type}`,
+    {
       next: { revalidate: 60 },
-    }),
+    },
   );
+  const data = await response.json();
 
-  if (!response) {
-    console.error("~ Fetch Error: Failed to get selected coffees.", fetchError);
-    return null;
-  }
-
-  const { error: parseError, result: data } = await Try<SelectedCoffees>(response.json());
-
-  if (parseError) {
-    console.error("~ Server Error: Failed to parse selected coffees data.", parseError);
-    return null;
-  }
+  // const { error: fetchError, result: response } = await Try(
+  //   fetch(`${process.env.NEXT_PUBLIC_API_URL}/item/selected/${type}`, {
+  //     next: { revalidate: 60 },
+  //   }),
+  // );
+  //
+  // if (!response) {
+  //   console.error("~ Fetch Error: Failed to get selected coffees.", fetchError);
+  //   return null;
+  // }
+  //
+  // const { error: parseError, result: data } = await Try<SelectedCoffees>(response.json());
+  //
+  // if (parseError) {
+  //   console.error("~ Server Error: Failed to parse selected coffees data.", parseError);
+  //   return null;
+  // }
 
   return data;
 };
