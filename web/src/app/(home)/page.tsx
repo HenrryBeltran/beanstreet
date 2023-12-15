6cca37008e29f94d149e5f15d18003b19eca11c3
Henrry Beltrán <henrry26bt@gmail.com>
2023-12-15 09:47:34 -0500

    feat(web): Remove noStore() from home page

--- web/src/app/(home)/page.tsx
+++ web/src/app/(home)/page.tsx
@@ -2,11 +2,8 @@ import Footer from "@/components/Footer";
 import AboutUsOverview from "./AboutUsOverview";
 import Hero from "./Hero";
 import SelectedCoffees from "./SelectedCoffees";
-import { unstable_noStore as noStore } from "next/cache";
 
 export default function HomePage() {
-  noStore();
-
   return (
     <main>
       <Hero />
