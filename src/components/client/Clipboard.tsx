4a9337c346a4190783bc2c8278e264d8ca265df7
Henrry Beltrán <henrry26bt@gmail.com>
2024-02-07 19:11:37 -0500

    feat: Add tooltip to copy email on hero section

--- src/components/client/Clipboard.tsx
+++ src/components/client/Clipboard.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 type Props = {
@@ -9,14 +10,30 @@ type Props = {
 };
 
 export default function Clipboard({ children, className, value }: Props) {
+  const [tooltip, setTooltip] = useState("Copy");
+
+  async function checkClipboard() {
+    const clip = await navigator.clipboard.readText();
+
+    if (clip === value) {
+      setTooltip("Copied!");
+    } else {
+      setTooltip("Copy");
+    }
+  }
+
   return (
     <span
-      className={twMerge("cursor-pointer", className)}
-      onClick={() => {
-        navigator.clipboard.writeText(value);
+      className={twMerge("group relative cursor-pointer", className)}
+      onClick={async () => {
+        await navigator.clipboard.writeText(value);
+        checkClipboard();
       }}
     >
       {children}
+      <span className="pointer-events-none absolute top-1/2 z-10 mr-14 inline-block -translate-y-1/2 rotate-90 rounded-md border border-stone-600 bg-stone-700 px-2 py-4 font-medium text-stone-50 opacity-0 transition-colors duration-700 group-hover:opacity-100">
+        {tooltip}
+      </span>
     </span>
   );
 }
